8773eec19a9e5af2f3c21e9129281857a8f0e0de
Shu-Otake <is0661vv@ed.ritsumei.ac.jp>
2024-07-03 20:37:55 +0900

    fixed errors

--- color_detection.py
+++ color_detection.py
@@ -7,11 +7,15 @@ class proccessImage:
         yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV) 
         clahe = cv2.createCLAHE(clipLimit = 2.0, tileGridSize = (8, 8)) 
         yuv[:, :, 0] = clahe.apply(yuv[:, :, 0]) 
-        img = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR) 
+        rgb = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR) 
 
-        return img
+        return rgb
 
 class detectBall:
+    lowColor = np.array([5, 150, 150])
+    highColor = np.array([15, 255, 255])
+    threshold = 0.005
+
     def extractSpecificColor(frame, lowColor, highColor):
         h, w, c = frame.shape
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -38,30 +42,31 @@ class detectBall:
 
 class display:
     def indicateCentroid(frame, pos):
-        centroid = cv2.circle(frame, (pos[0], pos[1]), 10, (255, 0, 0), -1)
-        cv2.imshow("Frame", centroid)
-
+        if pos is not None:
+            centroid = cv2.circle(frame, (pos[0], pos[1]), 10, (255, 0, 0), -1)
+            cv2.imshow("Frame", centroid)
+        else:
+            cv2.imshow("Frame", frame)
+        
 def main():
-    lowColor = np.array([5, 150, 150])
-    highColor = np.array([15, 255, 255])
-    areaRatioThreshold = 0.005
-
     camera = cv2.VideoCapture(0)
     while True:
         ret, frame = camera.read()
-        pos = detectBall.calculateCentroid(frame, areaRatioThreshold, lowColor, highColor)
-        display.indicateCentroid(frame, pos)
-
+        #frame = cv2.imread("ball_sample.png")
+        hsv, contours = detectBall.extractSpecificColor(frame, detectBall.lowColor, detectBall.highColor)
+        pos = detectBall.calculateCentroid(hsv, contours, detectBall.threshold)
         print("centroid of ball:")
         print(pos)
+        """
+        display.indicateCentroid(frame, pos)
 
         key = cv2.waitKey(1)
-
         if key == 27:
             break
+        """
 
     camera.release()
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
